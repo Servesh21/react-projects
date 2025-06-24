df5f7a4933a434e96ea4b76628ef1438df63836e
servesh21 <serveshkhade68@gmail.com>
2025-06-25 00:48:00 +0530

    modal added/Redux-Toolkit

--- Redux-ToolKit/src/App.jsx
+++ Redux-ToolKit/src/App.jsx
@@ -3,15 +3,18 @@ import CartContainer from "./components/CartContainer";
 import NavBar from "./components/Navbar";
 import { useEffect } from "react";
 import { calculateTotals } from "./features/cart/cartSlice";
+import Modal from "./components/Modal";
 
 function App() {
   const {cartItems}=useSelector((store)=>store.cart);
+  const {isOpen}= useSelector((store)=> store.modal)
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(calculateTotals())
   },[cartItems])
   return(
   <main>
+   {isOpen&&<Modal/>} 
     <NavBar/>
     <CartContainer/>
   </main>
